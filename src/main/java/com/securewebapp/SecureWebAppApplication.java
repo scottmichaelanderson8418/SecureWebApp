bda01731cd08c8f709d648ffd5d73da5d30abf32
scottmichaelanderson8418 <81242311+scottmichaelanderson8418@users.noreply.github.com>
2024-03-19 21:54:24 -0400

    Add files via upload

---
+++ src/main/java/com/securewebapp/SecureWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.securewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWebAppApplication.class, args);
+	}
+
+}
